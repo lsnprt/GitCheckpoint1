17f912bf60b73b582129f9141a38d6a61889ac65
Alessandro Peretti <alessandroperetti.se@gmail.com>
2023-02-16 14:19:28 +0100

    Added Calculate class, called its method in Program

--- GitCheckpoint1/Program.cs
+++ GitCheckpoint1/Program.cs
@@ -7,6 +7,8 @@
             Console.WriteLine("This is the Git checkpoint task 1.1");
 
             CheckTest.TestMessage("Task 2 checked");
+
+            Console.WriteLine(Calculate.Sum(3, 2));
         }
     }
 }
